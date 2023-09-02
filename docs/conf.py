ad998254e12865d451f919841efeb61ca03bfa83
Nikita Sobolev <mail@sobolevn.me>
2023-09-02 13:47:27 +0300

    Update deps (#111)
    * Update deps
    
    * Update deps
    
    * Update deps
    
    * Fix CI
    
    * Fix CI
    
    * Fix test cases

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@
 import os
 import sys
 
-import tomlkit
+import tomli
 
 sys.path.insert(0, os.path.abspath('..'))
 
@@ -24,9 +24,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 def _get_project_meta():
     with open('../pyproject.toml') as pyproject:
-        file_contents = pyproject.read()
-
-    return tomlkit.parse(file_contents)['tool']['poetry']
+        return tomli.load(pyproject)['tool']['poetry']
 
 
 pkg_meta = _get_project_meta()
@@ -43,7 +41,7 @@ release = version
 # -- General configuration ---------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-needs_sphinx = '2.3'
+needs_sphinx = '5.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -96,7 +94,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
